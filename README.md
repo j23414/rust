0c36112ad1710bd1f189ff2914f334b2980c52a6
j23414 <jenchang@iastate.edu>
2023-04-02 14:01:46 -0700

    Fix code
    * Allocate bitvec once based on sequence length
    * Use an i iterator and set ACGT to 1000, 0100, 0010, 0001 respectively
    * Print out the sequence and the new bitvec to compare
    * Add ambiguous nucelotide base codes (e.g. R = 1010, N = 1111)

--- README.md
+++ README.md
@@ -12,11 +12,24 @@ Install libraries (crates?)
 
 ```
 source "$HOME/.cargo/env"
+brew install gsl
 cargo install rust-bio-tools 
 ```
 
 ## Run program
 
 ```
-rustc main.rs
+# cargo new for_fun --bin # hmm
+cargo run
+
+    Finished dev [unoptimized + debuginfo] target(s) in 0.14s
+     Running `target/debug/hello_world`
+[65, 67, 71, 84, 65, 67, 71, 84]
+10000100001000011000010000100001
+[67, 71, 67, 71, 67, 71, 71, 71]
+01000010010000100100001000100010
 ```
+
+## Pick a profiler
+
+options: rustup, cargo flamegraph, heaptrack, valgrind, massif, ...
--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,88 @@
 extern crate bio;
 extern crate bit_vec;
 
-use bio::io::fasta;
+use bio::io::fasta::{Reader, Record};
 use bit_vec::BitVec;
 
 fn main() {
     let filename = "data/example.fasta";
-    let reader = fasta::Reader::from_file(filename).unwrap();
+    let reader = Reader::from_file(filename).unwrap();
 
     for result in reader.records() {
         let mut record = result.unwrap();
         let sequence = record.seq();
-        let mut bit_vec = BitVec::new();
 
+        // Allocate memory once then fill in values
+        let mut bit_vec = BitVec::from_elem(record.seq().len()*4, false);
+// 
+        let mut i=0;
         for base in sequence.iter() {
-            match *base {
-                b'A' => bit_vec.extend_from_slice(&[true, false, false, false]),
-                b'C' => bit_vec.extend_from_slice(&[false, true, false, false]),
-                b'G' => bit_vec.extend_from_slice(&[false, false, true, false]),
-                b'T' => bit_vec.extend_from_slice(&[false, false, false, true]),
+            match *base { // ACGT
+                b'A' => bit_vec.set(i,true),
+                b'C' => bit_vec.set(i+1,true),
+                b'G' => bit_vec.set(i+2,true),
+                b'T' => bit_vec.set(i+3,true),
+                // Ambiguity codes for nucleotide degeneracy
+                b'R' => { // AG
+                    bit_vec.set(i,true);
+                    bit_vec.set(i+2,true);
+                },
+                b'Y' => { // CT
+                    bit_vec.set(i+1,true);
+                    bit_vec.set(i+3,true);
+                },
+                b'K' => { // GT
+                    bit_vec.set(i+2,true);
+                    bit_vec.set(i+3,true);
+                },
+                b'M' => { // AC
+                    bit_vec.set(i,true);
+                    bit_vec.set(i+1,true);
+                },
+                b'S' => { // GC
+                    bit_vec.set(i+1,true);
+                    bit_vec.set(i+2,true);
+                },
+                b'W' => { // AT
+                    bit_vec.set(i,true);
+                    bit_vec.set(i+3,true);
+                },
+                b'B' => { // CGT
+                    bit_vec.set(i+1,true);
+                    bit_vec.set(i+2,true);
+                    bit_vec.set(i+3,true);
+                },
+                b'D' => { // AGT
+                    bit_vec.set(i,true);
+                    bit_vec.set(i+2,true);
+                    bit_vec.set(i+3,true);
+                },
+                b'H' => { // ACT
+                    bit_vec.set(i,true);
+                    bit_vec.set(i+1,true);
+                    bit_vec.set(i+3,true);
+                },
+                b'V' => { // ACG
+                    bit_vec.set(i,true);
+                    bit_vec.set(i+1,true);
+                    bit_vec.set(i+2,true);
+                },
+                b'N' => { // ACGT
+                    bit_vec.set(i,true);
+                    bit_vec.set(i+1,true);
+                    bit_vec.set(i+2,true);
+                    bit_vec.set(i+3,true);
+                },
                 _ => panic!("Invalid base: {}", base),
             }
+            i+=4;
         }
 
-        record.set_desc(String::from_utf8_lossy(record.desc()).to_string());
-        record.set_seq(bit_vec);
+        //record.set_desc(String::from_utf8_lossy(record.desc()).to_string());
+        //record.set_seq(bit_vec);
 
         println!("{:?}", record.seq());
+        println!("{:?}", bit_vec);
     }
 
 }
